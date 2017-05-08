8f9f822225fa8fd6854c56dcb080e37435e09c2b
Huelekim <hue1996mta@gmail.com>
2017-05-09 01:48:14 +0700

    Huệ_ Bảng Nhân Viên
    Model
    control

---
+++ QUANLY_BHST/CONTROL/NHANVIEN_C.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MODAL.ENNITES;
+using MODAL.FUNSIONS;
+using System.Data;
+
+namespace CONTROL
+{
+  public  class NHANVIEN_C
+    {
+        NHANVIEN_M obj_m = new NHANVIEN_M();
+        public DataTable Get_Obj()
+        {
+            return obj_m.Get_Obj();
+        }
+        public bool Add_Obj(NHANVIEN Obj)
+        {
+            return obj_m.Add_Obj(Obj);
+        }
+        public bool Up_Obj(NHANVIEN Obj)
+        {
+            return obj_m.Up_Obj(Obj);
+        }
+        public bool Del_Obj(string Obj)
+        {
+            return obj_m.Del_Obj(Obj);
+        }
+    }
+
+}
+
